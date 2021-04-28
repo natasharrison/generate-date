c247269ec4e85e6e5b3bc35fe028cc6c852d8257
samuel Odubamowo <wemsamuel@gmail.com>
2021-04-28 05:01:49 -0700

    image now pops up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,67 @@
 var renderRecipe = document.getElementById("get-recipe");
 var showRecipe = document.getElementById("recipe-info");
-
+var addCocktail = document.getElementById("drink-checkbox").checked;
 // click listener for get-recipe 
 renderRecipe.addEventListener("click", function (data) {
     // api for recipe
+    // we should maybe think about making the API key just foods, i see alot of dessets comming up 
     fetch('https://api.spoonacular.com/recipes/random?number=1&apiKey=2edea6ac2b434b7ca50d4616af915f07')
         .then(function (response) {
             console.log(response);
-            return response.json()
-
-                .then(function (data) {
-                    console.log(data)
-                    displayDinnerRecipe(data);
-                })
-                
+            return response.json();
         })
+        .then(function (data) {
+            console.log(data)
+            displayDinnerRecipe(data);
 
-    fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php')
-        .then(function (response) {
-            console.log(response);
-            return response.json()
+        if (addCocktail === true)    {}
+
+        
+
+        })
     
+
+
+});
+
+fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php')
+.then(function (response) {
+    console.log(response);
+    return response.json()
+
         .then(function (data) {
             console.log(data);
             displayCocktail(data);
         })
-    })
-});
+})
 
-var displayDinnerRecipe = function (data){
+
+
+var displayDinnerRecipe = function (data) {
     var recipes = data.recipes
     var ingredientsContainer = document.getElementById("recipe-ingredients")
 
-    for (var i = 0; i<recipes[0].extendedIngredients.length; i++){
+
+    for (var i = 0; i < recipes[0].extendedIngredients.length; i++) {
         var currentIngredient = document.createElement("li")
         currentIngredient.textContent = recipes[0].extendedIngredients[i].originalString;
         ingredientsContainer.appendChild(currentIngredient)
     }
 
     // need to display this as an image
-    document.getElementById("recipe-img").innerHTML = recipes[0].image
-    document.getElementById("recipe-title").innerHTML = recipes[0].title
-    document.getElementById("recipe-summary").innerHTML = recipes[0].summary
-    document.getElementById("recipe-steps").innerHTML = recipes[0].instructions
+    document.getElementById("main-recipe-img").src = recipes[0].image;
+    document.getElementById("recipe-title").innerHTML = recipes[0].title;
+    document.getElementById("recipe-summary").innerHTML = recipes[0].summary;
+    document.getElementById("recipe-steps").innerHTML = recipes[0].instructions;
 };
 
-var displayCocktail = function (data){
-    var drinks = data.drinks 
-    
+var displayCocktail = function (data) {
+    var drinks = data.drinks
+
     var drinkValues = Object.values(drinks[0])
-        drinkValues = drinkValues.slice(17,32)
-        drinkValues = drinkValues.filter(function(drink){
-        if (drink !== null){
+    drinkValues = drinkValues.slice(17, 32)
+    drinkValues = drinkValues.filter(function (drink) {
+        if (drink !== null) {
             return drink
         }
     })
@@ -59,7 +69,7 @@ var displayCocktail = function (data){
 
     var cocktailIngredients = document.getElementById("cocktail-ingredients")
 
-    for (var i = 0; i<drinkValues.length; i++){
+    for (var i = 0; i < drinkValues.length; i++) {
         var currentIngredient = document.createElement("li")
         currentIngredient.textContent = drinkValues[i];
         cocktailIngredients.appendChild(currentIngredient)
